4ca0080215224baca593ab88b89b38c8ce80de71
faazdy <leorengifor@gmail.com>
2025-02-11 17:11:04 -0500

    -m 'dark'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Navbar() {
+    //menu
+    const [open, setOpen] = useState(false)
+    const handleOpenMenu = ()=>{
+        setOpen(!open)
+    }
+
+    //dark-theme
+    const [isDark, setIsDark] = useState(localStorage.getItem("theme") === 'dark');
+
+    useEffect(()=>{
+        if(isDark){
+            document.documentElement.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark')
+        } else {
+            document.documentElement.classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light')
+        }
+    }, [isDark])
+
+    const toggleDark = () =>{
+        setIsDark(!isDark)
+    }
   return (
     <header>
         <nav>
@@ -16,7 +39,7 @@ function Navbar() {
             </div>
             <div className="theme">
                 <label className="ui-switch">
-                    <input type="checkbox" id="dark"/>
+                    <input type="checkbox" id="dark" onChange={toggleDark}/>
                     <div className="slider">
                         <div className="circle"></div>
                     </div>
@@ -26,8 +49,8 @@ function Navbar() {
                 <label htmlFor="burger-btn">
                     <img src="assets/icons/menu.png" alt="menu"/>
                 </label>
-                <input type="checkbox" name="burger-btn" id="burger-btn"/>
-                <nav className="burger-tags" id="burger-tags">
+                <input type="checkbox" name="burger-btn" id="burger-btn" onChange={handleOpenMenu}/>
+                <nav className="burger-tags" id="burger-tags" style={{transform: open ? 'scale(1)' : 'scale(0)'}}>
                     <a href="#projects">Projects</a>
                     <a href="#about">About me</a>
                     <a href="#contact">Contact</a>
