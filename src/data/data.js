31c707c3c18bcecc01d70b35b41e8a04ab170d48
faazdy <leorengifor@gmail.com>
2026-03-15 00:18:26 -0500

    Add PostgreSQL SVG icons for enhanced visual representation
    - Added postgresql.svg icon file with detailed vector graphics.
    - Introduced postgresqlCARD.svg icon file for card-style representation.

--- src/data/data.js
+++ src/data/data.js
@@ -2,9 +2,9 @@
 import {v4 as uuid} from "uuid";
 
 //PROJECT STACK ICONS
-import HTML from "/assets/icons/htmlCARD.svg"
+import PsSQL from "/assets/icons/postgresqlCARD.svg"
 import CSS from "/assets/icons/cssCARD.png"
-import JS from "/assets/icons/jsCARD.svg"
+import Express from "/assets/icons/expressCARD.svg"
 import React from "/assets/icons/reactCARD.svg"
 import Sass from "/assets/icons/sassCARD.svg"
 import Bootstrap from "/assets/icons/boostrapCARD.png"
@@ -14,10 +14,14 @@ import Vue from "/assets/icons/vueCARD.svg"
 import HTMLICON from "/assets/icons/html-5-logo-svgrepo-com.svg"
 import CSSICON from "/assets/icons/css.svg"
 import JSICON from "/assets/icons/js-svgrepo-com.svg"
+import NODEICON from "/assets/icons/nodejs.svg"
 import SASSICON from "/assets/icons/sass-svgrepo-com.svg"
 import bootstrapICON from "/assets/icons/boostrap-svg.svg"
 import REACTICON from "/assets/icons/react-svgrepo-com.svg"
 import SQLICON from "/assets/icons/SQLICON.svg"
+import DOCKERICON from "/assets/icons/docker.svg"
+import EXPRESSICON from "/assets/icons/express.svg"
+import POSTGRESQLICON from "/assets/icons/postgresql.svg"
 import MOTION from "/assets/icons/motion.png"
 import WORDPRESS from "/assets/icons/wp.png"
 import TSICON from "/assets/icons/ts.png"
@@ -27,13 +31,13 @@ import VUEICON from "/assets/icons/vue.svg"
 
 export const projects = [
     {
-        id: uuid(),
-        nameProject: 'UI Komponents',
-        description: 'UI Komponents is a platform that showcases a collection of buttons, shadows, and cards for user interfaces. The website allows users to preview and copy the code for each component, making it easy to integrate them into any web development project.',
-        stack:[React, Sass],
-        techStack: ["React", "Sass"],
-        cardImg: '/assets/content/projects/uikomponents.png',
-        url: "https://uikomponents.vercel.app/"
+       id: uuid(),
+        nameProject: 'PinGym - Gym Saas & Fitness App',
+        description: 'PinGym is a management application for gyms (Gym as a Service). It allows a gym to manage its facilities, clients, membership plans, workout routines, attendance, lockers, and communications, and it allows clients to register (including via QR), view their routines, progress, and attendance.',
+        stack:[Vue, CSS, Express, PsSQL],
+        techStack: ["Vue", "CSS", "Express", "PostgreSQL"],
+        cardImg: "/assets/content/projects/pingym.png",
+        url: 'https://pingym.vercel.app/' 
     },
     {
         id: uuid(),
@@ -55,21 +59,12 @@ export const projects = [
     },
     {
         id: uuid(),
-        nameProject: 'Shopping List App',
-        description: "The application helps users manage a shopping list by allowing them to add items, mark them as purchased, and delete individual items or the entire list. The list is saved in the browser's local storage to persist data between sessions.",
-        stack:[HTML, CSS, JS],
-        techStack: ["HTML", "CSS", "JavaScript"],
-        cardImg: '/assets/content/projects/shopping.png',
-        url: "https://mercado-list-fzdy.netlify.app/"
-    },
-    {
-        id: uuid(),
-        nameProject: 'Tesla Landing Page Clone',
-        description: "This project is a detailed replica of Tesla's official landing page. It captures the design and layout of the original site, offering an immersive visual experience that mirrors the aesthetic and branding of Tesla.",
-        stack:[HTML, CSS, Sass, JS],
-        techStack: ["HTML", "CSS", "Sass", "JavaScript"],
-        cardImg: '/assets/content/projects/tesla.png',
-        url: "https://tesla-laning-clone.netlify.app"
+        nameProject: 'UI Komponents',
+        description: 'UI Komponents is a platform that showcases a collection of buttons, shadows, and cards for user interfaces. The website allows users to preview and copy the code for each component, making it easy to integrate them into any web development project.',
+        stack:[React, Sass],
+        techStack: ["React", "Sass"],
+        cardImg: '/assets/content/projects/uikomponents.png',
+        url: "https://uikomponents.vercel.app/"
     },
     {
         id: uuid(),
@@ -115,22 +110,37 @@ export const stackLanguages = [
     },
     {
         id: uuid(),
-        nameTechnology: "Motion",
-        icon: MOTION
-    },
-    {
-        id: uuid(),
-        nameTechnology: "SQL",
-        icon: SQLICON
-    },
-    {
-        id: uuid(),
         nameTechnology: "Vue",
         icon: VUEICON
     },
     {
         id: uuid(),
-        nameTechnology: 'TypeScript',
-        icon: TSICON
+        nameTechnology: "Motion",
+        icon: MOTION
+    },
+    {
+        id: uuid(),
+        nameTechnology: "Node.js",
+        icon: NODEICON
+    },
+    {
+        id: uuid(),
+        nameTechnology: "Express",
+        icon: EXPRESSICON
+    },
+    {
+        id: uuid(),
+        nameTechnology: "MySQL",
+        icon: SQLICON
+    },
+    {
+        id: uuid(),
+        nameTechnology: 'Docker',
+        icon: DOCKERICON
+    },
+    {
+        id: uuid(),
+        nameTechnology: 'PostgreSQL',
+        icon: POSTGRESQLICON
     },
 ];
